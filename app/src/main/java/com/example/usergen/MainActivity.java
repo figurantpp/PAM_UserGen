3607d5e7f9144f21aebca040d67556f553514c7f
BuildTools <unconfigured@null.spigotmc.org>
2021-03-09 13:19:43 -0300

    Updated animatiions

--- app/src/main/java/com/example/usergen/MainActivity.java
+++ app/src/main/java/com/example/usergen/MainActivity.java
@@ -1,27 +1,45 @@
 package com.example.usergen;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-import com.example.usergen.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
 
+    Button search;
+
+    TextView title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.countries, android.R. layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        search = findViewById(R.id.search_on_api_button);
+
+        title = findViewById(R.id.random_text);
+
+
+        Animation updown = AnimationUtils.loadAnimation(this, R.anim.up_down);
+        Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.fade);
+
+        search.setAnimation(updown);
+
+        title.setAnimation(animation1);
 
     }
 }
