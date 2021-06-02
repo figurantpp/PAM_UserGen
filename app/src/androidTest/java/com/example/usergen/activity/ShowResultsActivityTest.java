f43fb2defb1e103a0c3b971117bed2fc6bf3ebe9
figurantpp <renan.r.marcelino2@protonmail.com>
2021-06-02 17:53:51 -0300

    Added nationality click test

--- app/src/androidTest/java/com/example/usergen/activity/ShowResultsActivityTest.java
+++ app/src/androidTest/java/com/example/usergen/activity/ShowResultsActivityTest.java
@@ -2,16 +2,22 @@ package com.example.usergen.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.widget.ImageView;
 
 import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.example.usergen.R;
+import com.example.usergen.model.LambdaMatcher;
 import com.example.usergen.model.user.RandomUserGeneratorInput;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +25,9 @@ import org.junit.runner.RunWith;
 import java.util.stream.Stream;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
@@ -46,15 +54,20 @@ public class ShowResultsActivityTest {
     @Rule
     public ActivityScenarioRule<ShowResultsActivity> rule = new ActivityScenarioRule<>(getIntent());
 
+    @Before
+    public void before() {
+        Intents.init();
+    }
+
+    @After
+    public void after(){
+        Intents.release();
+    }
+
     @Test
     public void testWorking() {
 
-        try {
-            Thread.sleep(2000);
-        }
-        catch (InterruptedException ex) {
-            //
-        }
+        sleep();
 
         Stream.of(
                 R.id.personFirstandSecondName,
@@ -68,11 +81,39 @@ public class ShowResultsActivityTest {
                 R.id.personEmail
         ).forEach(this::checkNotEmpty);
 
+        onView(withId(R.id.personPicture)).check(
+                matches(
+                        new LambdaMatcher<>(
+                                view -> ((ImageView) view).getDrawable() != null, ""
+                        )
+                )
+        );
+
+
+    }
+
+
+    @Test
+    public void testNationalityClick() {
+
+        sleep();
+
+        onView(withId(R.id.personNacionality)).perform(click());
+
+        intended(IntentMatchers.anyIntent());
 
     }
 
     private void checkNotEmpty(@IdRes int id) {
         onView(withId(id)).check(matches(not(withText(""))));
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            //
+        }
     }
 
 }
