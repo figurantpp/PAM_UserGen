b117c9dea66452c5ad126f3aeb86cb205db38a36
Júlia Braz <63269350+zulia21@users.noreply.github.com>
2021-06-12 17:04:22 -0300

    recycler view implement layout

---
+++ app/src/main/java/com/example/usergen/activity/ShowVariousUsersActivity.java
@@ -0,0 +1,20 @@
+package com.example.usergen.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.usergen.R;
+
+
+public class ShowVariousUsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_various_users);
+
+
+        
+    }
+}
--- app/src/main/java/com/example/usergen/model/view/UserListAdapter.java
+++ app/src/main/java/com/example/usergen/model/view/UserListAdapter.java
@@ -1,8 +1,11 @@
 package com.example.usergen.model.view;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.usergen.R;
 import com.example.usergen.model.user.User;
 
+import java.io.IOException;
 import java.util.List;
 
 public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserViewHolder> {
@@ -43,12 +47,32 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserVi
 
     static class UserViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView userItemName, userItemNationality;
+        public ImageView userItemImage;
+
         public UserViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            userItemName = itemView.findViewById(R.id.txtUserItemName);
+
+            userItemImage = itemView.findViewById(R.id.userItemImage);
+
+            userItemNationality = itemView.findViewById(R.id.txtUserItemNationality);
         }
 
-        public void bind(User user) {
+        public void bind(User user){
 
+            userItemName.setText(user.getName());
+
+            userItemNationality.setText(user.getNationality());
+
+            try {
+                userItemImage.setImageBitmap(user.getProfileImage().getBitmap());
+            }
+            catch (IOException exception)
+            {
+                Log.e("adapter", "shh");
+            }
         }
     }
 }
